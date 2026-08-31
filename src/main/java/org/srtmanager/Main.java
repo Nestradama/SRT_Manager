a3fb7dafb1e6d132000e1c8a722f9a1b1c168ba2
Corentin <98367308+Nestradama@users.noreply.github.com>
2026-08-31 23:42:09 +0200

    Chore: Added JavaFX dependency for the GUI Added gitattributes

--- src/main/java/org/srtmanager/Main.java
+++ src/main/java/org/srtmanager/Main.java
@@ -1,7 +1,11 @@
 package org.srtmanager;
 
+import org.srtmanager.cli.CliApp;
+
 public class Main {
     static void main(String[] args) {
+        CliApp.cliMode();
             System.out.println("SRT Manager CLI");
+
         }
     }
